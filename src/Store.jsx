34951e803d37a02e5490a478f3a99d911053da56
muddassirali8089 <114619571+muddassirali8089@users.noreply.github.com>
2025-06-15 14:45:30 +0500

    adding a store using reducer
    adding a store using reducer

---
+++ src/Store.jsx
@@ -0,0 +1,39 @@
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case "account/deposit":
+      return {
+        ...state,
+        balance: state.balance + action.payload,
+      };
+
+    case "account/withdraw":
+      return {
+        ...state,
+        balance: state.balance - action.payload,
+      };
+
+    case "account/requestLoan":
+      // Only one loan at a time
+      if (state.loan > 0) return state;
+
+      return {
+        ...state,
+        loan: action.payload.amount,
+        loanPurpose: action.payload.purpose,
+        balance: state.balance + action.payload.amount,
+      };
+
+    case "account/payLoan":
+      if (state.balance < state.loan) return state;
+
+      return {
+        ...state,
+        balance: state.balance - state.loan,
+        loan: 0,
+        loanPurpose: "",
+      };
+
+    default:
+      return state;
+  }
+}
